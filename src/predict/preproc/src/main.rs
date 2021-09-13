164bab63e5dbb4ccff8ede740fb94640c7ed257c
Joshua Tanner <mindful.jt@gmail.com>
2021-09-13 14:29:57 +0900

    get github shortcodes

--- src/predict/preproc/src/main.rs
+++ src/predict/preproc/src/main.rs
@@ -1,34 +1,46 @@
 use std::collections::HashMap;
 use fst::{Map, Error};
+use std::num::ParseIntError;
 
 #[derive(Debug, Clone)]
-struct ParseError;
-
-fn parse_github_emoji_url(url: String) -> Result<String, ParseError> {
-    let bytecode_strings: Vec<&str> = url.split('/')
-        .last().ok_or(ParseError)?
-        .split('.').next().ok_or(ParseError)?
-        .split('-').collect();
-
-    Ok(String::from("Dog"))
+enum ParseError {
+    InvalidJson(String),
+    InvalidHex(ParseIntError),
+    InvalidCodepoint(u32)
 }
 
-fn main() {
-    let test_str = String::from("https://github.githubassets.com/images/icons/emoji/unicode/1f44d.png?v8");
-    let test_other_Str = String::from("https://github.githubassets.com/images/icons/emoji/unicode/1f1e6-1f1fd.png?v8");
-    let l = test_str.split('/').last().unwrap().split('.').next().unwrap();
-    let ll: Vec<&str> = l.split("-").collect();
+//https://stackoverflow.com/questions/69152223/unicode-codepoint-to-rust-string
+fn parse_unicode(input: &str) -> Result<char, ParseError> {
+    let unicode = u32::from_str_radix(input, 16).map_err(ParseError::InvalidHex)?;
+    char::from_u32(unicode).ok_or_else(|| ParseError::InvalidCodepoint(unicode))
+}
 
+fn parse_github_emoji_url(url: &String) -> Result<String, ParseError> {
+    let bytecode_strings = url.split('/')
+        .last().ok_or(ParseError::InvalidJson(url.clone()))?
+        .split('.').next().ok_or(ParseError::InvalidJson(url.clone()))?.split('-');
 
+    bytecode_strings.map(|codepoint| parse_unicode(codepoint))
+    .collect::<Result<Vec<_>, _>>().map(|char_vec|char_vec.into_iter().collect::<String>())
+}
+
+fn github_emoji_shortcodes() -> Vec<(String, String)> {
     let json: HashMap<String, String> = ureq::get("https://api.github.com/emojis").call()
         .unwrap()
         .into_json()
         .unwrap();
 
-    let emoji_data: HashMap<_, _> = json.iter()
-        .map(|(key, url)| {(key.clone(), url)})
-        .collect();
-    println!("requested");
+    //have to filter out bad URLs like
+    // "https://github.githubassets.com/images/icons/emoji/bowtie.png?v8"
+    json.iter().filter_map(|(key, url)| {
+            parse_github_emoji_url(url).map(|unicode_str| (key.clone(), unicode_str)).ok()
+    }).collect::<Vec<(String, String)>>()
+}
+
+fn main() {
+    let shortcodes = github_emoji_shortcodes();
+
+    println!("debug");
 }
 
 
