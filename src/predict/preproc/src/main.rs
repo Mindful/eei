99e64dccb8b790662f24e03ea2b0f2c58c254f3c
Joshua Tanner <mindful.jt@gmail.com>
2021-09-20 22:31:18 +0900

    dedup symbols, cleanup

--- src/predict/preproc/src/main.rs
+++ src/predict/preproc/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use fst::{MapBuilder, SetBuilder};
 use std::num::ParseIntError;
 use std::fs::File;
@@ -42,44 +42,55 @@ fn github_emoji_shortcodes() -> Vec<(String, String)> {
 }
 
 
-fn write_symbols_and_shortcodes(data: &Vec<(String, String)>) -> Result<(), Box<dyn error::Error>> {
+fn write_symbols_and_shortcodes(mut shortcodes_symbols: Vec<(String, String)>) -> Result<(), Box<dyn error::Error>> {
     let writer = io::BufWriter::new(File::create("shortcodes.fst")?);
-    let mut build = MapBuilder::new(writer)?;
+    let mut map_builder = MapBuilder::new(writer)?;
 
+    shortcodes_symbols.sort();
 
-    let mut shortcodes_symbols = data.clone();
-    shortcodes_symbols.sort_by_key(|(shortcode, _symbol)| shortcode.clone());
-    let mut counter: u64 = 0;
-    let symbols: Vec<String> = shortcodes_symbols.into_iter()
-        .map(|(shortcode, content)| {
-            build.insert(shortcode, counter).map(|_| {
-                counter += 1;
-                content.clone()
-            })
-    }).collect::<Result<Vec<_>, _>>()?;
+    let symbols = shortcodes_symbols.iter()
+        .map(|(_shortcode, symbol)| { symbol })
+        .collect::<HashSet<&String>>();
+
+    let symbol_id_map: HashMap<&String, u64>  = symbols.iter()
+        .enumerate()
+        .map(|(idx, symbol)| { (*symbol, idx as u64) })
+        .collect();
+
+    for (shortcode, symbol) in shortcodes_symbols.iter() {
+        map_builder.insert(shortcode, *symbol_id_map.get(symbol).unwrap())?;
+    }
+
     // Finish construction of the map and flush its contents to disk.
-    build.finish()?;
+    map_builder.finish()?;
 
     let mut symbol_file = File::create("symbols.bin")?;
     symbol_file.write_all(&bincode::serialize(&symbols)?)?;
+
+    println!("Wrote {shortcodes} shortcodes for {symbols} symbols",
+             shortcodes=shortcodes_symbols.len(),
+             symbols=symbols.len());
+
     Ok(())
 }
 
 fn process_dictionary() -> Result<(), Box<dyn error::Error>> {
     let writer = io::BufWriter::new(File::create("dictionary.fst")?);
-    let mut build = SetBuilder::new(writer)?;
+    let mut set_builder = SetBuilder::new(writer)?;
 
     let mut lines = io::BufReader::new(File::open("hunspell_US.txt")?)
         .lines()
         .collect::<Result<Vec<_>, _>>()?;
 
+    //must be in lexographical order to build the FST
     lines.sort();
 
-    for line in lines {
-        build.insert(line)?;
+    for line in lines.iter() {
+        set_builder.insert(line)?;
     }
 
-    build.finish()?;
+    set_builder.finish()?;
+    println!("Wrote {entries} dictionary entries", entries=lines.len());
     Ok(())
 }
 
@@ -88,11 +99,10 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     let shortcodes = github_emoji_shortcodes();
 
     println!("Writing symbols and shortcodes to files");
-    write_symbols_and_shortcodes(&shortcodes)?;
+    write_symbols_and_shortcodes(shortcodes)?;
     println!("Processing dictionary");
     process_dictionary()?;
 
-    println!("debug");
     Ok(())
 }
 
