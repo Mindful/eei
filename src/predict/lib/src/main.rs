a71e947e00faa8404147050944e3658c5af36e72
Joshua Tanner <mindful.jt@gmail.com>
2021-09-20 14:20:31 +0900

    trying to build predictor

--- src/predict/lib/src/main.rs
+++ src/predict/lib/src/main.rs
@@ -1,5 +1,6 @@
-use fst::{Map, Set};
-
+use fst::{Map, Set, IntoStreamer};
+use fst::automaton::{Automaton, Str, Levenshtein};
+use lazy_static::lazy_static;
 
 struct Predictor {
     dictionary: Set<Vec<u8>>,
@@ -7,8 +8,36 @@ struct Predictor {
     symbols: Vec<String>
 }
 
+impl Predictor {
+    // fn word(&self, context: &str) {
+    //
+    // }
+
+    fn symbol(&self, context: &str) {
+        let context_matcher = Str::new(context).starts_with();
+        self.shortcode_dictionary.search(context_matcher)
+            .into_stream();
+    }
+}
+
+
+lazy_static! {
+    static PREDICTOR: Predictor = Predictor {
+        dictionary: Set::new(include_bytes!("../../dictionary.fst").to_vec()).unwrap(),
+        shortcode_dictionary: Map::new(include_bytes!("../../shortcodes.fst").to_vec()).unwrap(),
+        symbols: bincode::deserialize(include_bytes!("../../symbols.bin")).unwrap()
+    };
+}
+
+
+
 
 
 fn main() {
+    // File written from a build script using MapBuilder.
+    static FST: &[u8] = include_bytes!("../../dictionary.fst");
+
+    let map : Map<&[u8]> = Map::new(FST).unwrap();
+
     println!("lib main");
 }
