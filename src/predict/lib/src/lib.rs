b5f391cc25df362ca90ac059e788c9731db33e27
Joshua Tanner <mindful.jt@gmail.com>
2021-11-04 22:14:58 +0900

    start adding word handling, but buggy

--- src/predict/lib/src/lib.rs
+++ src/predict/lib/src/lib.rs
@@ -1,25 +1,25 @@
 #![allow(non_upper_case_globals)]
 mod predict;
 
-use std::ffi::{CString, CStr, NulError};
+use std::ffi::{CString, NulError, CStr};
 use std::os::raw::{c_char, c_int};
-use std::mem;
 use log::LevelFilter;
 use log4rs::append::file::FileAppender;
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::config::{Appender, Config, Root};
 
-use crate::predict::PredictionError::{FailedStringConversion, FstError, LevenshteinError, MissingSymbol};
-use crate::predict::PredictionError;
 use crate::predict::PREDICTOR;
-use ibus::{IBusEEIEngine, gboolean, GBOOL_FALSE, ibus_engine_update_lookup_table, IBusEngine, GBOOL_TRUE, ibus_engine_hide_lookup_table, guint, IBusModifierType_IBUS_RELEASE_MASK, IBusModifierType_IBUS_CONTROL_MASK, IBUS_e, IBUS_asciitilde, IBUS_space, IBUS_Return, IBUS_BackSpace, IBUS_Escape, IBUS_Page_Down, IBUS_Page_Up, ibus_engine_commit_text, ibus_text_new_from_unichar, ibus_text_new_from_string, gchar, ibus_lookup_table_clear, ibus_lookup_table_append_candidate, IBusText, ibus_lookup_table_append_label, ibus_engine_update_auxiliary_text, IBUS_Up, IBUS_Down, IBUS_Left, IBUS_Right, ibus_lookup_table_get_cursor_pos, IBusLookupTable, ibus_lookup_table_get_label, ibus_lookup_table_cursor_up, ibus_lookup_table_cursor_down, ibus_engine_hide_auxiliary_text, ibus_lookup_table_set_label, ibus_lookup_table_page_down, ibus_lookup_table_page_up, ibus_lookup_table_get_number_of_candidates, ibus_text_new_from_static_string, ibus_engine_show_lookup_table, ibus_lookup_table_get_cursor_in_page, gunichar, IBusModifierType_IBUS_SHIFT_MASK};
+use ibus::{IBusEEIEngine, gboolean, GBOOL_FALSE, ibus_engine_update_lookup_table, IBusEngine, GBOOL_TRUE, ibus_engine_hide_lookup_table, guint, IBusModifierType_IBUS_CONTROL_MASK, IBUS_e, IBUS_w, IBUS_asciitilde, IBUS_space, IBUS_Return, IBUS_BackSpace, IBUS_Escape, IBUS_Page_Down, IBUS_Page_Up, ibus_engine_commit_text, ibus_text_new_from_unichar, ibus_text_new_from_string, gchar, ibus_lookup_table_clear, ibus_lookup_table_append_candidate, IBusText, ibus_engine_update_auxiliary_text, IBUS_Up, IBUS_Down, ibus_lookup_table_get_cursor_pos, IBusLookupTable, ibus_lookup_table_get_label, ibus_lookup_table_cursor_up, ibus_lookup_table_cursor_down, ibus_engine_hide_auxiliary_text, ibus_lookup_table_set_label, ibus_lookup_table_page_down, ibus_lookup_table_page_up, ibus_lookup_table_get_number_of_candidates, ibus_text_new_from_static_string, ibus_engine_show_lookup_table, ibus_lookup_table_get_cursor_in_page, gunichar, IBusModifierType_IBUS_SHIFT_MASK, ibus_lookup_table_get_candidate};
 use std::cmp::min;
+use lazy_static::lazy_static;
 
+lazy_static! {
+    static ref empty_cstring: CString = CString::new("").unwrap();
+}
 
 pub struct EngineCore {
     lookup_visible: bool,
     word_buffer: String,
-    cursor_pos: i32,
     symbol_input: bool,
     symbol_preedit: String,
     symbol_label_vec: Vec<CString>,
@@ -32,7 +32,6 @@ pub unsafe extern "C" fn new_engine_core(parent_engine: *mut IBusEEIEngine) -> *
     Box::into_raw(Box::new(EngineCore {
         lookup_visible: false,
         word_buffer: String::new(),
-        cursor_pos: 0,
         symbol_input: false,
         symbol_preedit: String::new(),
         symbol_label_vec: Vec::new(),
@@ -66,7 +65,7 @@ impl EngineCore {
             let page_num = idx / page_size;
             if self.symbol_last_page != page_num {
                 self.symbol_last_page = page_num;
-                for (idx, table_idx) in (page_num * page_size..min((page_size * (page_size+1)), self.symbol_label_vec.len() as u32)).enumerate() {
+                for (idx, table_idx) in (page_num * page_size..min(page_size * (page_size+1), self.symbol_label_vec.len() as u32)).enumerate() {
                     ibus_lookup_table_set_label(self.get_table(), idx as guint, ibus_text_new_from_static_string(self.symbol_label_vec.get_unchecked(table_idx as usize).as_ptr()))
                 }
             }
@@ -74,13 +73,70 @@ impl EngineCore {
         ibus_engine_update_lookup_table(self.parent_engine_as_ibus_engine(), self.get_table(), GBOOL_TRUE);
     }
 
+    unsafe fn word_table_update(&mut self) -> gboolean {
+        if !self.lookup_visible || self.symbol_input {
+            return GBOOL_FALSE;
+        }
+        else if self.word_buffer.is_empty() {
+            return self.word_table_disable();
+        }
+
+        let search_result  = PREDICTOR.word(self.word_buffer.as_str());
+        match search_result {
+            Ok(candidates) => {
+                log::info!("Word search for {} and got {:?}", self.word_buffer, candidates);
+                let table = self.get_table();
+                ibus_lookup_table_clear(table);
+                for word in candidates {
+                    match CString::new(word.into_bytes()) {
+                        Ok(cstring_word) => {
+                            ibus_lookup_table_append_candidate(table, ibus_text_new_from_string(cstring_word.into_raw() as *mut gchar));
+                        }
+                        Err(err) => {
+                            log::error!("Failed string conversion for word lookup: {}", err);
+                        }
+                    }
+                }
+                ibus_engine_update_lookup_table(self.parent_engine_as_ibus_engine(), table, GBOOL_TRUE);
+                GBOOL_TRUE
+            }
+            Err(err) => {
+                log::error!("{}", err);
+                GBOOL_FALSE
+            }
+        }
+    }
+
+    unsafe fn word_table_enable(&mut self) -> gboolean {
+        if self.lookup_visible || self.word_buffer.is_empty() {
+            return GBOOL_FALSE;
+        }
+
+        self.lookup_visible = true;
+        let ret = self.word_table_update();
+        ibus_lookup_table_clear(self.get_table());
+        ibus_engine_show_lookup_table(self.parent_engine_as_ibus_engine());
+        ret
+    }
+
+    unsafe fn word_table_disable(&mut self) -> gboolean {
+        if !self.lookup_visible {
+            return GBOOL_FALSE;
+        }
+
+        self.lookup_visible = false;
+        ibus_engine_hide_lookup_table(self.parent_engine_as_ibus_engine());
+        GBOOL_TRUE
+    }
+
     unsafe fn symbol_input_enable(&mut self) -> gboolean {
-        if self.symbol_input {
+        if self.lookup_visible {
             return GBOOL_FALSE;
         }
 
         self.symbol_input = true;
         self.lookup_visible = true;
+        ibus_lookup_table_clear(self.get_table());
         ibus_engine_show_lookup_table(self.parent_engine_as_ibus_engine());
         GBOOL_TRUE
     }
@@ -95,6 +151,9 @@ impl EngineCore {
         self.symbol_preedit.clear();
         ibus_engine_hide_lookup_table(self.parent_engine_as_ibus_engine());
         ibus_engine_hide_auxiliary_text(self.parent_engine_as_ibus_engine());
+        for i in 0..(*self.get_table()).page_size {
+            ibus_lookup_table_set_label(self.get_table(), i, ibus_text_new_from_static_string(empty_cstring.as_ptr()));
+        }
         GBOOL_TRUE
     }
 
@@ -110,7 +169,7 @@ impl EngineCore {
                 ibus_engine_update_auxiliary_text(self.parent_engine_as_ibus_engine(), ibus_string, GBOOL_TRUE);
             }
             Err(err) => {
-                log::error!("Failed string conversion for symbol aux text update");
+                log::error!("Failed string conversion for symbol aux text update: {}", err);
             }
         }
 
@@ -153,6 +212,33 @@ impl EngineCore {
         GBOOL_TRUE
     }
 
+    unsafe fn word_commit(&mut self) -> gboolean {
+        if !self.lookup_visible || self.symbol_input {
+            return GBOOL_FALSE;
+        }
+
+        let idx = ibus_lookup_table_get_cursor_in_page(self.get_table());
+        let candidate = ibus_lookup_table_get_candidate(self.get_table(), idx);
+        match CStr::from_ptr((*candidate).text as *const c_char).to_str() {
+            Ok(word) => {
+                match into_ibus_string(String::from(&word[self.word_buffer.len()..])) {
+                    Ok(ibus_word) => {
+                        ibus_engine_commit_text(self.parent_engine_as_ibus_engine(), ibus_word);
+                    }
+                    Err(err) => {
+                        log::error!("Failed to convert slice back into ibus string: {}", err);
+                    }
+                }
+            }
+            Err(err) => {
+                log::error!("Failed to convert word to string for commit: {}", err);
+            }
+        }
+
+        self.word_buffer.clear();
+        self.word_table_disable()
+    }
+
     unsafe fn symbol_input_commit(&mut self) -> gboolean {
         if !self.symbol_input {
             log::error!("Symbol input commit called outside symbol input mode");
@@ -161,7 +247,7 @@ impl EngineCore {
 
         let idx = ibus_lookup_table_get_cursor_in_page(self.get_table());
         let symbol = ibus_lookup_table_get_label(self.get_table(), idx);
-        ibus_engine_commit_text(self.parent_engine as *mut IBusEngine, symbol);
+        ibus_engine_commit_text(self.parent_engine_as_ibus_engine(), symbol);
 
         self.symbol_input_disable()
     }
@@ -187,7 +273,7 @@ pub struct SymbolPredictions {
     shortcodes: *mut *mut c_char
 }
 
-
+#[allow(unused_variables)]
 #[no_mangle]
 pub unsafe extern "C" fn ibus_eei_engine_process_key_event(engine: *mut IBusEngine, keyval: guint,
     keycode: guint, modifiers: guint) -> gboolean {
@@ -203,28 +289,44 @@ pub unsafe extern "C" fn ibus_eei_engine_process_key_event(engine: *mut IBusEngi
     };
 
 
-    if (modifiers & IBusModifierType_IBUS_CONTROL_MASK) == IBusModifierType_IBUS_CONTROL_MASK && keyval == IBUS_e {
-        log::info!("enable symbol input");
-        return engine_core.symbol_input_enable();
+    if (modifiers & IBusModifierType_IBUS_CONTROL_MASK) == IBusModifierType_IBUS_CONTROL_MASK {
+        //control key (and only control key) is held down
+        return match keyval {
+            IBUS_e => {
+                engine_core.symbol_input_enable()
+            }
+            IBUS_w => {
+                engine_core.word_table_enable()
+            }
+            _ => {
+                GBOOL_FALSE
+            }
+        }
     } else if (modifiers & !IBusModifierType_IBUS_SHIFT_MASK) != 0 {
         return GBOOL_FALSE; //This also covers released keys with IBUS_RELEASE_MASK
     }
 
     match keyval {
         IBUS_space => {
-            //TODO: handle aborting words, not just symbols
             if engine_core.symbol_input {
                 engine_core.symbol_input_disable();
+            } else if engine_core.lookup_visible {
+                engine_core.word_table_disable();
             }
-            engine_core.commit_char(keyval)
+            let ret = engine_core.commit_char(keyval);
+            engine_core.word_buffer.clear();
+            ret
         }
         IBUS_Return => {
-            //TODO also handle committing words, not just symbols
-            if engine_core.symbol_input {
+            let ret = if engine_core.symbol_input {
                 engine_core.symbol_input_commit()
+            } else if engine_core.lookup_visible {
+                engine_core.word_commit()
             } else {
                 engine_core.commit_char(keyval)
-            }
+            };
+            engine_core.word_buffer.clear();
+            ret
         }
         IBUS_Up => {
             if engine_core.lookup_visible {
@@ -248,6 +350,11 @@ pub unsafe extern "C" fn ibus_eei_engine_process_key_event(engine: *mut IBusEngi
             if engine_core.symbol_input {
                 engine_core.symbol_preedit.pop();
                 return engine_core.symbol_input_update();
+            } else {
+                if engine_core.lookup_visible {
+                    engine_core.word_buffer.pop();
+                    engine_core.word_table_update();
+                }
             }
             GBOOL_FALSE
         }
@@ -280,7 +387,11 @@ pub unsafe extern "C" fn ibus_eei_engine_process_key_event(engine: *mut IBusEngi
                 engine_core.symbol_preedit.push((keyval as u8) as char);
                 engine_core.symbol_input_update()
             } else {
-                engine_core.commit_char(keyval)
+                let ret = engine_core.commit_char(keyval);
+                if engine_core.lookup_visible {
+                    engine_core.word_table_update();
+                }
+                ret
             }
         }
         _ => GBOOL_FALSE
@@ -302,38 +413,5 @@ pub unsafe extern "C" fn configure_logging() {
 
     log::info!("Logging initialized");
 }
-
-//based on
-////https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=d0e44ce1f765ce89523ef89ccd864e54
-fn convert_string_vector(str_vec: Vec<String>) -> *mut *mut c_char {
-    str_vec.into_iter().map(|s| {
-        CString::new(s.into_bytes()).map(|cstr| cstr.into_raw())
-    }).collect::<Result<Vec<_>, _>>().map(|mut cstring_vec| {
-        let ptr = cstring_vec.as_mut_ptr();
-        mem::forget(cstring_vec);
-        ptr
-    }).unwrap_or_else(|_| {
-        log::error!("Failed to convert string vector");
-        std::ptr::null_mut()
-    })
-}
-
-unsafe fn free_string_array(ptr: *mut *mut c_char, len: c_int) {
-    let len = len as usize;
-
-    // Get back our vector.
-    // Previously we shrank to fit, so capacity == length.
-    let v = Vec::from_raw_parts(ptr, len, len);
-
-    // Now drop one string at a time.
-    for elem in v {
-        let s = CString::from_raw(elem);
-        mem::drop(s);
-    }
-
-    // Afterwards the vector will be dropped and thus freed.
-}
-
-
 
 
